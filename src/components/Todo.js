7e9ae1ae6ac6b2c8e67dcd0e725ba67ec55e36d4
souravgreatest <jainsourav220@gmail.com>
2025-04-29 17:01:54 +0530

    Completed assignment

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,28 +3,24 @@ import React, { useEffect, useState } from 'react';
 const Todo = ({ data }) => {
   const [item, setItem] = useState(data);
 
-  // Update local state if parent sends new data
   useEffect(() => {
     setItem(data);
   }, [data]);
 
-  // Delete handler
   const handleDelete = (index) => {
     const updatedItems = item.filter((_, i) => i !== index);
     setItem(updatedItems);
   };
 
   return (
-    <div>
-      {
-        item.map((ele, id) => (
-          <div key={id} style={{ marginTop: '10px' }}>
-            <span style={{ marginRight: '500px' }}>{ele}</span>
-            <button onClick={() => handleDelete(id)}>Delete</button>
-          </div>
-        ))
-      }
-    </div>
+    <ul>
+      {item.map((ele, id) => (
+        <li key={id} style={{ display: 'flex', justifyContent: 'space-between', marginTop: '10px' }}>
+          <span>{ele}</span>
+          <button onClick={() => handleDelete(id)}>Delete</button>
+        </li>
+      ))}
+    </ul>
   );
 };
 
